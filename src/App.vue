838eaf5f88deee4b886c26618793c83c020cd2a4
taiju59 <aokitaiju@gmail.com>
2019-04-24 21:38:37 +0900

    ボタンでタスク追加

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <h1>Lovely Vue</h1>
     <input v-model="message" />
+    <button @click="add">追加</button>
     <br />
     <ul>
       <li v-for="todo in todos" :key="todo.id" class="task-card">
@@ -17,7 +18,17 @@ export default {
   data: () => ({
     todos: [{ id: 1, text: "ときめき" }],
     message: "Spark joy!"
-  })
+  }),
+  methods: {
+    add() {
+      const newTodo = {
+        id: this.todos.length + 1,
+        text: this.message
+      };
+      this.todos.push(newTodo);
+      this.message = "";
+    }
+  }
 };
 </script>
 
