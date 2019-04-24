275e6229a2aa10400389679d39f7b8929bc7d129
taiju59 <aokitaiju@gmail.com>
2019-04-24 21:31:04 +0900

    dataの表示(宣言的レンダリング)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
     <h1>Lovely Vue</h1>
+    {{ message }}
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data: () => ({
+    message: "Spark joy!"
+  })
 };
 </script>
 
